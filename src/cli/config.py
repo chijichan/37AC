32f6f18b91c366b216d112cc364334acc46a0e9f
fagong jiyi <yangxiuhong54895@163.com>
2026-02-13 23:14:35 +0800

    修改配置

--- src/cli/config.py
+++ src/cli/config.py
@@ -11,14 +11,15 @@ AUTO_DEVICE = True
 DEVICE = None
 
 # 训练相关配置
-DATASET_DIR = Path("E:/pj/TAC_dataset/processed")  # 建议也改为Path对象，或者保持字符串
+DATASET_DIR = Path("E:/pj/TAC_dataset/processed")
+# DATASET_DIR = Path("W:/Img")
 
 MODEL_SAVE_PATH = ROOT_PATH / "saves" / "models" / "character_resnet18.pth"  # 模型
 CLASSES_TXT_PATH = ROOT_PATH / "saves" / "models" / "classes.txt"  # 类别名
 
-NUM_EPOCHS = 10
-BATCH_SIZE = 16
-IMAGE_SIZE = 224
+NUM_EPOCHS = 100
+BATCH_SIZE = 16  # 16
+IMAGE_SIZE = 256  # 224
 LEARNING_RATE = 1e-4
 
 # 预测相关配置
@@ -33,6 +34,9 @@ HEARTBEAT_INTERVAL_SEC = 10  # 心跳发送间隔（与线程一致）
 HEARTBEAT_RESPONSE_TIMEOUT_SEC = 30  # 超过该时间未收到 heartbeat_ack 则认为超时
 HEARTBEAT_MISS_LIMIT = 3  # 允许连续丢失 heartbeat_ack 的最大次数
 RECONNECT_DELAY_SEC = 10  # 重连前等待时间（秒）
+
+# 暂存
+IMAGE_PATH = ROOT_PATH / "saves" / "uploads"
 
 # 日志
 LOGS_PATH = ROOT_PATH / "logs"
